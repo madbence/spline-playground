011942c7c4ca439a81931d8ae8933249bd330282
Bence Dányi <madbence@gmail.com>
2014-03-06 22:40:06 +0100

    Add dev server

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var browserify = require('browserify-middleware');
+var stylus = require('stylus');
+var fs = require('fs');
+
+var app = express();
+
+app.get('/bundle.js', browserify('./lib/app.js', {
+  transform: ['jadeify']
+}));
+
+app.get('/style.css', function(req, res) {
+  fs.readFile('./style/style.styl', 'utf-8', function(err, str) {
+    if(err) {
+      return req.next(err);
+    }
+    stylus.render(str, {
+      filename: './style/style.styl'
+    }, function(err, css) {
+      if(err) {
+        return req.next(err);
+      }
+      res.type('css');
+      res.send(css);
+    });
+  });
+});
+
+app.get('/*', function(req, res) {
+  res.render('index.jade');
+});
+
+app.listen(process.env.PORT || 3000);
