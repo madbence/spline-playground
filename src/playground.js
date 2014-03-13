2609441a95f7bc3676ac81a77affff7f0e5be572
Bence Dányi <madbence@gmail.com>
2014-03-13 01:15:13 +0100

    Refactor view layer

---
+++ src/playground.js
@@ -0,0 +1,58 @@
+var Class = require('./class');
+var Dzhugashvili = require('./spline/dzhugashvili');
+var SimpleOutline = require('./renderer/simple-outline');
+var mouse = require('./util/mouse');
+
+function devnull(e) {
+  e.stopPropagation && e.stopPropagation();
+  e.preventDefault && e.preventDefault();
+  return false;
+}
+
+var Playground = Class.extend({
+  initialize: function(el) {
+
+    this.ctx = el.getContext('2d');
+    var rect = el.getBoundingClientRect();
+    this.ctx.translate(-rect.left, -rect.top);
+
+    var doc = el.ownerDocument;
+    doc.addEventListener('contextmenu', devnull);
+
+    el.addEventListener('click', mouse(0, this.add, this));
+
+    this.loop = this.loop.bind(this);
+
+    this.points = [];
+
+    this.spline = new Dzhugashvili(this.points);
+
+    this.renderer = [];
+    this.renderer.push(new SimpleOutline(this.ctx));
+
+    this.loop();
+  },
+  loop: function() {
+    //try {
+      this.draw();
+    //} catch(ex) {
+    //  throw ex;
+    //}
+    window.requestAnimationFrame(this.loop);
+  },
+  add: function add(x, y) {
+    this.points.push({
+      x: x, y: y, t: Date.now()
+    });
+    this.spline.recompile();
+  },
+  draw: function draw() {
+    if(this.points.length < 2) return;
+    this.ctx.clearRect(0, 0, 1000, 1000);
+    for(var i = 0; i < this.renderer.length; i++) {
+      this.renderer[i].draw(this.spline);
+    }
+  }
+});
+
+module.exports = Playground;
