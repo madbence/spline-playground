f1b4589a4653db475a9a49b845984597e1aa9533
Bence Dányi <madbence@gmail.com>
2014-03-13 11:08:05 +0100

    Add knot renderer

---
+++ src/renderer/knot.js
@@ -0,0 +1,28 @@
+var Class = require('../class');
+
+var Knot = Class.extend({
+  initialize: function(ctx, opt) {
+    opt = opt || {};
+    this.ctx = ctx;
+    this.fillStyle = opt.fillStyle = '#666';
+    this.strokeStyle = opt.strokeStyle = '#fff';
+    this.lineWidth = opt.lineWidth = 1;
+    this.radius = opt.radius || 3;
+  },
+  draw: function(spline) {
+    var ctx = this.ctx;
+    ctx.save();
+    ctx.strokeStyle = this.strokeStyle;
+    ctx.fillStyle = this.fillStyle;
+    ctx.lineWidth = this.lineWidth;
+    for(var i = 0; i < spline.points.length; i++) {
+      ctx.beginPath();
+      ctx.arc(spline.points[i].x, spline.points[i].y, this.radius, 0, Math.PI*2);
+      ctx.fill();
+      ctx.stroke();
+    }
+    ctx.restore();
+  }
+});
+
+module.exports = Knot;
